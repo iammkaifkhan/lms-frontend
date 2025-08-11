53141e6c40a8be9a4f7c7071db3e37036b10db34
iammkaifkhan <iammkaifkhan@gmail.com>
2025-08-11 14:24:47 +0530

    initial frontend

--- src/Constants/CelebrityData.js
+++ src/Constants/CelebrityData.js
@@ -1,5 +1,5 @@
 import apj from "../Assets/Images/QuotesPersonalityImage/apj.png"
-import billgates from "../Assets/Images/QuotesPersonalityImage/billgates.png"
+import billgates from "../Assets/Images/QuotesPersonalityImage/billGates.png"
 import einstein from "../Assets/Images/QuotesPersonalityImage/einstein.png"
 import nelsonmandela from "../Assets/Images/QuotesPersonalityImage/nelsonmandela.png"
 import stevejobs from "../Assets/Images/QuotesPersonalityImage/stevejobs.png"
@@ -11,25 +11,25 @@ export const celebrities = [
         image: nelsonmandela,
         slideNumber: 1
     },
-     {
+    {
         title: "APJ Abudul Kalam",
         description: "You have to dream before your dreams can come true.",
         image: apj,
         slideNumber: 2
     },
-     {
+    {
         title: "Albert Einstein",
         description: "Imagination is more important than knowledge.",
         image: einstein,
         slideNumber: 3
     },
-     {
+    {
         title: "Steve Jobs",
         description: "Innovation distinguishes between a leader and a follower.",
         image: stevejobs,
         slideNumber: 4
     },
-     {
+    {
         title: "Bill Gates",
         description: "I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.",
         image: billgates,
